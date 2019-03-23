30175206c328eb8a6f4a1c718e5596ea84d83f6d
SashaZaicev <sasha167@bk.ru>
2019-03-23 22:34:36 +0300

    git commit -m

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+     var arr = str.split('');
+    var brack = bracketsConfig;
+    for(var i=0;i<arr.length;i++){
+      
+      for(var j=0; j<brack.length; j++){
+        if(arr[i] === brack[j][0] && arr[i+1] === brack[j][1]){
+          arr.splice(i,2);
+          if (arr.length==0){ 
+              return true;
+          }
+          i=-1;
+        }
+      }
+    }
+    return false;
 }
+
